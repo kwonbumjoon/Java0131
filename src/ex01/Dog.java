57a72d83c3219a211b52c266653334d0847ee105
kwonbumjoon <bumjoon2010@gmail.com>
2024-01-31 12:17:29 +0900

    first commit

---
+++ src/ex01/Dog.java
@@ -0,0 +1,7 @@
+package ex01;
+
+public class Dog extends Animal {
+	void bark() {
+		System.out.println("멍멍하고 짖다.");
+	}
+}
---
+++ src/ex04/Zoo.java
@@ -0,0 +1,14 @@
+package ex04;
+
+public class Zoo {
+	public static void main(String[] args) {
+		Dog objdog = new Dog();
+		objdog.makeSound();
+
+		Cat objcat = new Cat();
+		objcat.makeSound();
+		
+		Bird objbird = new Bird();
+		objbird.makeSound();
+	}
+}
